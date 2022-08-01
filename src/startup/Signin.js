74dd872178bdfabc4fcb00576ecae37cc78a05e2
Vetrivelu Murugesan <75613547+vetrivelu@users.noreply.github.com>
2022-08-01 19:56:27 +0530

    added web client and ios client

--- src/startup/Signin.js
+++ src/startup/Signin.js
@@ -356,9 +356,9 @@ export default class Signin extends Component<Props> {
   
     GoogleSignin.configure({
       // scopes: ['https://www.googleapis.com/auth/drive.readonly'], // what API you want to access on behalf of the user, default is email and profile
-      webClientId: '168494380414-j7533q2p386s46srnktvf830cdcb4ujs.apps.googleusercontent.com', // client ID of type WEB for your server (needed to verify user ID and offline access)
+      webClientId: '815732641901-olm1lfidlumvv0eu73otd7aqhb8g51cm.apps.googleusercontent.com', // client ID of type WEB for your server (needed to verify user ID and offline access)
       offlineAccess: true, // if you want to access Google API on behalf of the user FROM YOUR SERVER
-      iosClientId: '168494380414-62teeq7udk6d6kvjd8datldrpqkicn0m.apps.googleusercontent.com', // [iOS] optional, if you want to specify the client ID of type iOS (otherwise, it is taken from GoogleService-Info.plist)
+      iosClientId: '815732641901-eodu974594qbgvfgim85580slf0h6gpo.apps.googleusercontent.com', // [iOS] optional, if you want to specify the client ID of type iOS (otherwise, it is taken from GoogleService-Info.plist)
       forceCodeForRefreshToken: true,
 
   
